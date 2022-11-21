0142bdf808ba9ab6f811f33631f2637e34c1e747
Titu Mollick <117148051+TituMollick@users.noreply.github.com>
2022-11-21 22:32:29 +0600

    Add files via upload

---
+++ TituMollick_07/js/custom.js
@@ -0,0 +1,28 @@
+/*
+    Titu Mollick
+    https://titumallick.com/
+    Portfolio Website	
+*/
+//
+$(document).ready(function() {
+			$('#fullpage').fullpage({
+				'verticalCentered': false,
+				'scrollingSpeed': 600,
+				'autoScrolling': false,
+				'css3': true,
+				'navigation': true,
+				'navigationPosition': 'right',
+			});
+		});
+
+// wow
+$(function()
+{
+    new WOW().init();
+    $(".rotate").textrotator();
+})
+/*
+    Titu Mollick
+    https://titumallick.com/
+    Portfolio Website	
+*/
